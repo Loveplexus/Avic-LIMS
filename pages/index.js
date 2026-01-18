b4df84a3c99323d67921988bb93fc3d8269ba6be
Loveplexus <daramolaayodeji2@gmail.com>
2026-01-17 23:28:49 -0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Open LIMS Home</h1>
+      <p>Go to <a href="/login">Login</a> to access your lab system</p>
+    </div>
+  )
+}
